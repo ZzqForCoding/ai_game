643bc6261ccfda277e2a1ad89f0d0432fb068378
周子清 <17687952609@163.com>
2022-09-03 22:36:32 +0800

    code runner

--- thrift/code_running.thrift
+++ thrift/code_running.thrift
@@ -4,6 +4,8 @@ struct Bot {
     1: i32 userId,
     2: string botCode,
     3: string input,
+    4: string language,
+    5: string room_name,
 }
 
 service CodeRunning {
---
+++ thrift/message.thrift
@@ -0,0 +1,5 @@
+namespace py message
+
+service Message {
+    i32 response(1: string info)
+}
