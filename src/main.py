e5c819dd72cedff20a1d7c1b50ec5c4420d19ede
周子清 <17687952609@163.com>
2022-10-30 14:17:15 +0800

    new code runner

---
+++ src/main.py
@@ -0,0 +1,54 @@
+from code_running_service import CodeRunning
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+from sandbox import Sandbox
+
+class CodeRunningHandler:
+    def start_container(self, uuid, code, lang):
+        if Sandbox.bots.get(uuid) == None:
+            Sandbox.bots[uuid] = Sandbox(uuid, code, lang)
+        return 0
+
+    def stop_container(self, uuid):
+        sandbox = Sandbox.bots.get(uuid)
+        if sandbox is not None:
+            sandbox.stop()
+            del Sandbox.bots[uuid]
+        return 0
+
+    def compile(self, uuid):
+        sandbox = Sandbox.bots.get(uuid)
+        if sandbox is not None:
+            result = sandbox.compile()
+            if result != 'ok':
+                return result
+        return ""
+
+    def prepare_data(self, uuid, data):
+        sandbox = Sandbox.bots.get(uuid)
+        if sandbox is not None:
+            sandbox.prepare_data(data)
+        return 0
+
+    def run(self, uuid):
+        output = None
+        sandbox = Sandbox.bots.get(uuid)
+        if sandbox is not None:
+            output = sandbox.run().strip()
+        return output
+
+if __name__ == '__main__':
+    handler = CodeRunningHandler()
+    processor = CodeRunning.Processor(handler)
+    transport = TSocket.TServerSocket(host='172.17.0.4', port=9090)
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+    server = TServer.TThreadedServer(processor, transport, tfactory, pfactory)
+
+    print('Starting the server...')
+    server.serve()
+    print('done.')
