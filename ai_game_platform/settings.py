3a67574df336b60a47598f06aff3480124fde42b
子清 周 <17687952609@163.com>
2023-01-01 17:02:27 +0800

    update to mysql

--- ai_game_platform/settings.py
+++ ai_game_platform/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'ai_game_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3'
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'rm-wz962g459960y2opr.mysql.rds.aliyuncs.com',
-        'PORT': 3306,
-        'USER': 'aigame',
-        'PASSWORD': "Zcbm980528",
-        'NAME': 'aigame',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+        #'ENGINE': 'django.db.backends.mysql',
+        #'HOST': 'rm-wz962g459960y2opr.mysql.rds.aliyuncs.com',
+        #'PORT': 3306,
+        #'USER': 'aigame',
+        #'PASSWORD': "Zcbm980528",
+        #'NAME': 'aigame',
     }
 }
 
